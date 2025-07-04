9a8e0dc1dca4843469fc92b494f184d9de235bb7
egor <teryaev.egor28@gmail.com>
2025-07-04 15:23:27 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -74,27 +74,40 @@ function toggleTab(e) {
 
 
 // ====================================================
+// Аккордеон
 
-const accordionLists = document.querySelector('.accordion-list');
+    const accordionLists = document.querySelectorAll('.accordion-list');
 
-accordionLists.addEventListener('click', (e) => {
-    const accordionList = e.currentTarget; 
-    const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened');
-    const accordionOpenedContent = accordionOpenedItem.querySelector('.accordion-list__item--opened .accordion-list__control');
-    const accordionControl = e.target.closest('.accordion-list__control');
-    if (!accordionControl) return;
-    const accordionItem = accordionControl.parentElement;
-    const accordionContent = accordionControl.nextElementSibling;
+    accordionLists.forEach(el => {
 
-    if (accordionOpenedItem) {
-        accordionOpenedItem.classList.remove('accordion-list__item--opened');
-        accordionOpenedContent.style.maxHeight = null
-    }
-    accordionItem.classList.toggle('accordion-list__item--opened');
+        el.addEventListener('click', (e) => {
 
-    if (accordionItem.classList.contains('accordion-list__item--opened')) {
-        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-    } else {
-        accordionContent.style.maxHeight = null
-    }
-})
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            e.preventDefault()
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+
+        });
+
+    });
+
+
+
+
