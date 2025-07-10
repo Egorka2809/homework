bbb727cbf380a6b08b5c370e05ec5baef5fee4eb
egor <teryaev.egor28@gmail.com>
2025-07-10 11:39:28 +0300

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -164,8 +164,12 @@
                 slidesPerView: 2.1,
             }
         }
-
-
     });
+
+    // Маска для телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
 
 })()
