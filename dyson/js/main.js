6a8a079c714ab1f8356c049d9e7dc08c41052fca
egor <teryaev.egor28@gmail.com>
2025-08-09 14:45:27 +0300

    Add files via upload

--- dyson/js/main.js
+++ dyson/js/main.js
@@ -338,6 +338,29 @@
         });
     });
 
+    // ===========
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.write__button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+
 
     // Аккордеон
 
